a6f338fe98d09b2943684fc82b119c0f274064e1
Jordan Johnson <jordan.steven.johnson@okstate.edu>
2019-11-25 23:28:19 -0600

    Fixed Parser to save and load nparray files, basic network having errors from memory allocation

--- program/train_model_tf.py
+++ program/train_model_tf.py
@@ -4,30 +4,20 @@ CS4793
 @authors: Melanie Bischoff, Malay Bhakta, Jordan Johnson
 """
 
-import sys
-import json
-import os
-import math
-import random
 import argparse
+import json
 import logging
-import platform
-import cv2
-import datetime
-from tqdm import tqdm
-from PIL import Image
-from pathlib import Path
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
-#from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Add, Input, Concatenate
-#from tensorflow.keras import layers
-#from tensorflow.keras import optimizers
+import os
+# from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Add, Input, Concatenate
+# from tensorflow.keras import layers
+# from tensorflow.keras import optimizers
 from collections import defaultdict
-from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow import keras
+from tqdm import tqdm
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -56,22 +46,32 @@ def run_analysis():
     Classification ranking?  Print out stuff?
     :return:
     """
+    logging.info("Started analysis")
+    logging.info("Finished analysis")
 
-def test_model(model):
+def test_model(model, test_data, test_labels):
     """
 
     :param model:
+    :param test_data:
+    :param test_labels:
     :return:
     """
+    logging.info("Started test model")
+    model.evaluate(test_data, test_labels)
+    logging.info("Finished test model")
     return model
 
 def train_model(model, train_data, train_labels):
     """
-    look at the the xview2/baseline/model/damage_classification.py and xview2/baseline/model/model.py
 
     :param model:
-    :return: model
+    :param train_data:
+    :param train_labels:
+    :return:
     """
+    logging.info("Started train model")
+
     model.fit(train_data, train_labels, epochs=10)
 
     """
@@ -79,61 +79,50 @@ def train_model(model, train_data, train_labels):
     model.fit(train_data, train_labels, epochs=10,
          validation_data=(val_data, val_labels))
     """
+    logging.info("Finished train model")
 
     return model
 
 def build_model(input_shape):
     """
-    look at the the xview2/baseline/model/damage_classification.py and xview2/baseline/model/model.py
-    CNN?
-    How many layers?  What weights?  What method? Optimizer, etc.
+    What are the weights, optimizer, etc.??
 
-    :param data_dir:
-    :return: model
+    :param input_shape: Dynamic based on image data
+    :return:
     """
+    logging.info("Started build model")
+
+    model = keras.models.Sequential()
     """
-    model = keras.sequential([
-        keras.layers.Flatten(input_shape=(1024, 1024)),
-        keras.layers.Dense(128, activation=tf.nn.relu),
-        keras.layers.Dense(5, activation=tf.nn.softmax)
-    ])
-    """
-
-    #TODO: is this the right thing to start on?
-    model = models.Sequential()
-
-    #1024 x 1024 is how big the images are
-    #inputs = Input(shape=(1024, 1024))
-
     model.add(keras.layers.Flatten(input_shape=input_shape))
     model.add(keras.layers.Dense(128, activation=tf.nn.relu))
     model.add( keras.layers.Dense(5, activation=tf.nn.softmax))
+    """
 
-    """
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(1024, 1024)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    """
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_shape[1], input_shape[2], input_shape[3])))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+
 
     #print the output
-    model.summary()
+    #model.summary()
 
     #are more layers better?
-    """
+
     #Add Dense Layers
-    model.add(layers.Flatten())
-    model.add(layers.Dense(2048, activation='relu'))
-    #model.add(layers.Dense(1024, activation='relu'))
-    model.add(layers.Dense(512, activation='relu'))
-    #model.add(layers.Dense(128, activation='relu'))
-    model.add(layers.Dense(64, activation='softmax'))
-    model.add(layers.Dense(4, activation='relu'))
+    model.add(keras.layers.Flatten())
+    #model.add(keras.layers.Dense(2048, activation='relu'))
+    #model.add(keras.layers.Dense(1024, activation='relu'))
+    #model.add(keras.layers.Dense(512, activation='relu'))
+    #model.add(keras.layers.Dense(128, activation='relu'))
+    model.add(keras.layers.Dense(64, activation='softmax'))
+    model.add(keras.layers.Dense(5, activation='softmax'))
 
     #print the output
     model.summary()
-    """
+
     #TODO: add weights and generators and optimizers
 
     # adam optimizer
@@ -148,7 +137,9 @@ def build_model(input_shape):
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    #TODO: return model created;
+    logging.info("Finished build model")
+
+    #return model created
     return model
 
 def colorize_mask_(mask, color_map=None):
@@ -210,7 +201,7 @@ def load_json_and_img(data_dir, out_dir):
             label_path = img_path.replace('png', 'json').replace('images', 'labels')
             label_file = open(label_path)
             label_data = json.load(label_file)
-            damage_type = "no-damage"
+            damage_encorder = 0
 
             for feat in label_data['features']['xy']:
 
@@ -218,12 +209,14 @@ def load_json_and_img(data_dir, out_dir):
                 try:
                     damage_type = feat['properties']['subtype']
                     if damage_type != "no-damage":
+                        damage_encorder = damage_intensity_encoding[damage_type]
                         break
 
                 except:  # pre-disaster damage is default no-damage
+                    damage_encorder = damage_intensity_encoding["no-damage"]
                     continue
 
-            labels.append(damage_type)
+            labels.append(damage_encorder)
 
         images_array = np.asarray(images)
         labels_array = np.asarray(labels)
@@ -243,12 +236,11 @@ def main():
     :return:
     """
 
-    parser = argparse.ArgumentParser(description='Run XView2 Challenge Disaster Damage Classification Training & Evaluation')
+    parser = argparse.ArgumentParser(description='CS4793 Training Model')
     parser.add_argument('--data',
-                        #default='..\\..\\data',
                         default='C:/Dev/Workspaces/Python/CS4793/xview2/data',
                         #Malay's dir
-                        #default='C:/Dev/Workspaces/Python/CS4793/xview2/data',
+                        #default='D:/Fall2019/AI/train',
                         metavar="/home/scratch1/cs4793/data",
                         help="Full path to the parent data directory")
     parser.add_argument('--val_split_pct',
@@ -258,36 +250,33 @@ def main():
                         help="Percentage to use for validation")
     parser.add_argument('--out',
                         default='C:/Dev/Workspaces/Python/CS4793/xview2/out',
+                        #Malay's Dir
+                        #default='D:/Fall2019/AI/train/out',
                         metavar='Output directory',
                         help="Output directory")
     args = parser.parse_args()
 
     """
-    Maybe these should be the steps:
-        1. Identify & Mask the buildings for the pre disasters.
-        2. Use those 
+    1. Let's just start with seeing if we can train on a model to identify if anything is damaged or not
+    2. Identify buildings and what not.
+    3. Cake!
     """
-
-
 
     #load the images and labels
     image_array, label_array = load_json_and_img(args.data, args.out)
 
-    logging.info("Started build model")
+    #build the model
     model = build_model(image_array.shape)
-    logging.info("Finished build model")
 
-    logging.info("Started train model")
+    #train the model
     model = train_model(model, image_array, label_array)
-    logging.info("Finished train model")
 
-    logging.info("Started test model")
-    model = test_model(model)
-    logging.info("Finished test model")
+    #test the model
+    #model = test_model(model, test_data, test_labels)
 
-    logging.info("Started analysis")
-    run_analysis(model)
-    logging.info("Finished analysis")
+    #do some analysis
+    #run_analysis(model)
+
 
 
 if __name__ == "__main__":
